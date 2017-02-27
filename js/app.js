5c7e69a5007795f72d855f54892628f1b576e588
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2017-02-27 00:10:38 -0500

    added scoreboard, copyright for artist of art used, and some styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
   var turn = 2 ;
-
+  var player1Score = 0
+  var player2Score = 0
 
 
     $(".box").on("click", function playerPlays(event){
@@ -23,98 +24,167 @@ $(document).ready(function() {
             $('#box-1').text()==='X' &&
             $('#box-2').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-3').text()==='X' &&
                  $('#box-4').text()==='X' &&
                  $('#box-5').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-6').text()==='X' &&
                  $('#box-7').text()==='X' &&
                  $('#box-8').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-0').text()==='X' &&
                  $('#box-3').text()==='X' &&
                  $('#box-6').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-1').text()==='X' &&
                  $('#box-4').text()==='X' &&
                  $('#box-7').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-2').text()==='X' &&
                  $('#box-5').text()==='X' &&
                  $('#box-8').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-0').text()==='X' &&
                  $('#box-4').text()==='X' &&
                  $('#box-8').text()==='X'){
           alert("player one wins!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-2').text()==='X' &&
                  $('#box-4').text()==='X' &&
                  $('#box-6').text()==='X'){
           alert("Player One Wins!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
         }
         else if ($('#box-0').text()==='O' &&
                  $('#box-1').text()==='O' &&
                  $('#box-2').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player1Score++;
+          $('.player1ScoreTotal').text(player1Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-3').text()==='O' &&
                  $('#box-4').text()==='O' &&
                  $('#box-5').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-6').text()==='O' &&
                  $('#box-7').text()==='O' &&
                  $('#box-8').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-0').text()==='O' &&
                  $('#box-3').text()==='O' &&
                  $('#box-6').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-1').text()==='O' &&
                  $('#box-4').text()==='O' &&
                  $('#box-7').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-2').text()==='O' &&
                  $('#box-5').text()==='O' &&
                  $('#box-8').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-0').text()==='O' &&
                  $('#box-4').text()==='O' &&
                  $('#box-8').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if ($('#box-2').text()==='O' &&
                  $('#box-4').text()==='O'&&
                  $('#box-6').text()==='O'){
           alert("Player Two Wins!!!!!");
+          player2Score++;
+          $('.player2ScoreTotal').text(player2Score);
+          $(".box").text("");
+          turn = 2;
 
         }
         else if (turn%11===0){
             alert("Tie Game!!");
+            $(".box").text("");
+            turn = 2;
         }
 
     });
 
   $(".btn").on("click", function resetGame(event){
     $(".box").text("");
+    $(".player1ScoreTotal").text("0");
+    $(".player2ScoreTotal").text("0");
+
     turn =2;
 
 
